e8e663ec67ab709420315034e37e7308ea543405
kiet-trann <kiettuantranle@gmail.com>
2025-02-12 08:45:03 +0700

    update code

---
+++ learnez-fe/src/pages/DashboardAdminPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardAdmin from '../../containers/DashboardAdmin'
+
+const DashboardAdminPage = () => {
+  return (
+    <div>
+      <DashboardAdmin />
+    </div>
+  )
+}
+
+export default DashboardAdminPage
+
